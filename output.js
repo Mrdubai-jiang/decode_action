42ad01b5169f3d09ad80b030257438b06dc94c3f
action <action@github.com>
2024-09-03 07:37:56 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Sep 01 2024 07:02:30 GMT+0000 (Coordinated Universal Time)
+//Tue Sep 03 2024 07:37:56 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 NAME = "薏米短剧";
